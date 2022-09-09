0cc689218d5402122fa11b148fbc191d64cfbf17
JayHaji <jaymainframe@JayHajis-MacBook-Pro.local>
2022-09-09 10:23:03 -0400

    Initial Load

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { Story } from './storyBuilder.js'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+        <div id='navBar'>
+          <div id='logoHolder'></div><h1 id='reddit'>Reddit Client</h1>
+          <div id='dropSearch'>
+              <select>
+                <option>r/popular</option>
+                <option>r/popular</option>
+                <option>r/popular</option>
+                <option>r/popular</option>
+                <option>r/popular</option>
+              </select></div>
+     </div>
+        <div id='scrollCont'>
+          <Story />
+          <div className='storyBox'>
+           
+            <div className='rightStoryBox'>
+            <div className='topStoryBox'>
+            <div className='leftStoryBox'>
+             
+              <h2>14.3k</h2>
+              
+            </div>
+            <h1 className='title'>Title: moms are coming fast! and thre are more of them than there are of you</h1>
+            </div>
+            <div className='storyImage' style={{  
+  backgroundImage: undefined, height: '0px'
+
+}}></div>
+            <div className='bottomStoryBox'>
+              <h2>username</h2><h2>posted x hours ago</h2><h2>457 comments</h2>
+            </div>
+            </div>         
+          </div> 
+          <div className='storyBox'>
+            <h1>Title: moms are coming fast!</h1>
+            <div className='storyImage'>imagecontainer</div>
+            <h2 className='bottomStoryBox'>username, time posted ago, number of comments</h2>
+          </div>
+          <div className='storyBox'>
+            <h1>Title: moms are coming fast!</h1>
+            <div className='storyImage'>imagecontainer</div>
+            <h2>username, time posted ago, number of comments</h2>
+          </div>
+          <div className='storyBox'>
+            <h1>Title: moms are coming fast!</h1>
+            <div className='storyImage'>imagecontainer</div>
+            <h2>username, time posted ago, number of comments</h2>
+          </div>
+          <div className='storyBox'>
+            <h1>Title: moms are coming fast!</h1>
+            <div className='storyImage'>imagecontainer</div>
+            <h2>username, time posted ago, number of comments</h2>
+          </div>
+        </div>
+        
+        
     </div>
   );
 }
---
+++ src/storyBuilder.js
@@ -0,0 +1,26 @@
+
+
+export function Story() {
+    return (
+        <div className='storyBox'>
+           
+            <div className='rightStoryBox'>
+            <div className='topStoryBox'>
+            <div className='leftStoryBox'>
+              <h2 className='topArrow'>^</h2>
+              <h2>14.3k</h2>
+              <h2 className='bottomArrow'>^</h2>
+            </div>
+            <h1 className='title'>Title: moms are coming fast! and thre are more of them than there are of you</h1>
+            </div>
+            <div className='storyImage' style={{  
+  backgroundImage: "url(" + "https://images.pexels.com/photos/34153/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350" + ")",
+
+}}></div>
+            <div className='bottomStoryBox'>
+              <h2>username</h2><h2>posted x hours ago</h2><h2>457 comments</h2>
+            </div>
+            </div>         
+          </div> 
+    )
+}
